c8cadca14551ebc883cbe6375e73d5af8835f9b9
QuestTestAutomation <45171766+QuestTestAutomation@users.noreply.github.com>
2019-01-05 20:13:53 +0530

    Commiting changes

--- Dev/Utilities/ExceptionLib.py
+++ Dev/Utilities/ExceptionLib.py
@@ -1,5 +1,11 @@
 
 
-def HandleException(ErrorMessage,FunctionName):
+class HandleException():
 
-    print("Error : " + ErrorMessage + " ; raised while executing the function  " + FunctionName)
+    def __init__(self):
+
+        pass
+    @staticmethod
+    def handle_exception(self,ErrorMessage,FunctionName):
+
+        print("Error : " + ErrorMessage + " ; raised while executing the function  " + FunctionName)

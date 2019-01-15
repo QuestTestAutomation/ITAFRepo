f6f9f6b3357da66bfee3d57a639d9ef1d57c172a
QuestTestAutomation <45171766+QuestTestAutomation@users.noreply.github.com>
2019-01-15 20:12:43 +0530

    Commiting changes

--- Dev/Marketing/Scripts/Oasis Driver.py
+++ Dev/Marketing/Scripts/Oasis Driver.py
@@ -5,7 +5,7 @@ from ITAFRepo.Dev.Utilities import Seleniumutil
 from ITAFRepo.Dev.Siebel import Souilib
 from ITAFRepo.Dev.Excel import XLLib
 from ITAFRepo.Dev.Utilities import Seleniumutil
-from ITAFRepo.Dev.Marketing.Libs.PageObjectLibrary import contentmanagementpage
+from ITAFRepo.Dev.Marketing.Libs.PageObjectLibrary import *
 import time
 browser = 'gc'
 url = 'http://stage-o2/v2/Documents/Edit/152/'
@@ -15,14 +15,34 @@ Variablesfile  = 'C:/Users/sanumolu/Documents/QSTAFGdrive/VDI 1/ITAF/ITAFRepo/De
 runmanagerfile = 'C:/Users/sanumolu/Documents/QSTAFGdrive/VDI 1/ITAF/ITAFRepo/Dev/Files/Marketing/OasisRunManager.xlsx'
 runmanagersheet = 'RunManager'
 testdatafile = 'C:/Users/sanumolu/Documents/QSTAFGdrive/VDI 1/ITAF/ITAFRepo/Dev/Files/Marketing/OasisDataBank.xlsx'
-testdatasheet = 'DataBank'
+
 iniittaf = InitializeITAF.initializeITAF()
 globaldict = iniittaf.set_global_dictionary(Variablesfile)
 print('globaldict')
 print(globaldict)
 xllib = XLLib.XLLib()
+rundict = xllib.load_xl_cell_values_dictionary(runmanagerfile, runmanagersheet)
+print(rundict)
+for key,value in rundict.items():
+    print('key is ' + str(key))
+    print('value is ' + str(value))
+
+for row in range(2,XLLib.get_xl_row_count(runmanagersheet,runmanagersheet)):
+    print('row ')
+    print(str(row))
+    print(XLLib.get_table_row_as_dictionary(row, rundict))
+    run = XLLib.get_table_row_as_dictionary(row, rundict)
+    if
+    contentmanagement_page.add_whitepaper(XLLib.get_table_row_as_dictionary(row, tabledictinoary))
+    time.sleep(30)
+
+
+
+"""
+
+testdatasheet = 'DataBank'
 datadict = xllib.load_xl_cell_values_testrail_dictionary(testdatafile, testdatasheet)
-rundict = xllib.load_xl_cell_values_testrail_dictionary(runmanagerfile, runmanagersheet)
+
 print('Datadict')
 print(datadict)
 print('Rundict')
@@ -48,3 +68,4 @@ contentmanagement_page.click_on_whitepaper(whitepaper)
 
 print(seleniumutil.get_webpage_url())
 print('******')
+"""
